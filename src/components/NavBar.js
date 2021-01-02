bff0102493ff205c8949b0183436e7ab0cb1ec2c
Mahabubur Rahman <microronyy@gmail.com>
2021-01-02 07:46:10 +0600

    setted up empty scss

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,26 @@
+import { NavLink } from 'react-router-dom'
+
 const Navbar = () => {
-  return <h1>Navbar Page!</h1>
+  return (
+    <header className="main-header">
+      <div className="navbar container">
+        <nav>
+          <NavLink to="/" exact className="nav-link cursive">
+            Mahabubur
+          </NavLink>
+          <NavLink to="/post" className="nav-link">
+            Blog Posts
+          </NavLink>
+          <NavLink to="/projects" className="nav-link">
+            Projects
+          </NavLink>
+          <NavLink to="/about" className="nav-link">
+            About Me!
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  )
 }
 
 export default Navbar
