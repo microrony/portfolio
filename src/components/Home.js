d62b1910a83b1638afe0ea5d27e2acdade70a445
Mahabubur Rahman <microronyy@gmail.com>
2021-01-02 09:23:46 +0600

    completed the home component design

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,10 @@
 const Home = () => {
-  return <h1>Home Page!</h1>
+  return (
+    <div>
+      <img className='home-img' src="https://images.unsplash.com/photo-1515664952592-0891e0d5f2d0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="Sky"/>
+      <h1 className='my-name cursive'>Mahabubur Rahman</h1>
+    </div>
+  )
 }
 
 export default Home
