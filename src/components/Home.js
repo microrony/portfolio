552ffdc902b1762db3f813fc7f0086bae26885a3
Mahabubur Rahman <microronyy@gmail.com>
2021-01-02 10:01:49 +0600

    updated the img link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <img className='home-img' src="https://images.unsplash.com/photo-1515664952592-0891e0d5f2d0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="Sky"/>
+      <img className='home-img' src="https://images.unsplash.com/photo-1607794124823-51c36486bfa4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80" alt="Sky"/>
       <h1 className='my-name cursive'>Mahabubur Rahman</h1>
     </div>
   )
