47cb4c61f380765bba2bd8663ab6f18bc46a5e82
Mahabubur Rahman <microronyy@gmail.com>
2021-01-02 12:16:32 +0600

    make request to sanity cms and pull out my blog post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,51 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import sanityClient from '../client.js'
+
 const Post = () => {
-  return <h1>Post Page!</h1>
+  const [postData, setPostData] = useState(null)
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == 'post']{
+          title,
+          slug,
+          mainImage{
+            asset->{
+              _id,
+              url
+            },
+            alt
+          }
+        }`
+      )
+      .then(data => setPostData(data))
+      .catch(console.error)
+  }, [])
+
+  return (
+    <main>
+      <section>
+        <h1>Blog Posts Page</h1>
+        <h2>Welcome to my page of blog posts</h2>
+        <div>
+          { postData && postData.map((post, index) => (
+          <article>
+            <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+            <span key={index}>
+              <img src={post.mainImage.asset.url} alt={post.mainImage.alt}/>
+              <span>
+                <h3>{post.title}</h3>
+              </span>
+            </span>
+            </Link>
+          </article>
+))}
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default Post
