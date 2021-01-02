ace2407b1a2d5ff8879977d7f821365eabc4fffb
Mahabubur Rahman <microronyy@gmail.com>
2021-01-02 09:25:39 +0600

    editted react router path

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Route component={About} path='/about' />
       <Route component={SinglePost} path='/post/:slug' />
       <Route component={Post} path='/post' />
-      <Route component={Project} path='/project' />
+      <Route component={Project} path='/projects' />
     </Switch>
     </BrowserRouter>
   );
